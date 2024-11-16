a9550d23ed03e904e1dd893fa9b45e3cb5a39a2d
Konrad Leśkiewicz <konradrysiek@gmail.com>
2024-11-16 23:55:36 +0100

    lab03

---
+++ lab03/pba/src/main/java/zut/pba/PbaApplication.java
@@ -0,0 +1,13 @@
+package zut.pba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PbaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PbaApplication.class, args);
+    }
+
+}
---
+++ lab03/pba/src/test/java/zut/pba/PbaApplicationTests.java
@@ -0,0 +1,13 @@
+package zut.pba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PbaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
